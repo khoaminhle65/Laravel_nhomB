818533e7f2244f0b661d6516be42a085521d510b
tiendai78 <162559631+DoTienDai@users.noreply.github.com>
2024-03-31 01:01:40 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::get('list', [CrudUserController::class, 'listUser'])->name('user.list');
 Route::get('/', function () {
     return view('welcome');
 });
